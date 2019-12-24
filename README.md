90b7ef868aaf2b2a4287d4f17129d130358402e0
Mauricio Araldi <mauricio.araldi@linkit.nl>
2019-12-24 11:45:15 +0100

    Initial project configuration

---
+++ README.md
@@ -0,0 +1,11 @@
+# ParkIt 0.1.0
+
+Vehicle parking sensor simulation, where you implement the logic to the sensor in JS. A nice way to train your skills.
+
+## Disclaimer
+This project is licensed under GNU General Public License 3. See LICENSE file for more informations.
+
+No results are guaranteed. Use this software at your own risk.
+
+## Contributors
+- Mauricio Araldi, [@mauricioaraldi](https://github.com/mauricioaraldi/)
---
+++ webpack/webpack.common.js
@@ -0,0 +1,58 @@
+const Path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: Path.resolve(__dirname, '../static/index.js'),
+  output: {
+    path: Path.join(__dirname, '../dist'),
+    filename: '[name].js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: Path.resolve(__dirname, '../static/*.css'), to: '', flatten: true }
+    ]),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../pages/index.html'),
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '~': Path.resolve(__dirname, '../')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]'
+          }
+        }
+      },
+    ]
+  }
+};
